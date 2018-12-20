382f26d28ff65ee99b3064cfeee08f2227f98fd6
Gahozipauline <gashozipauly@gmail.com>
2018-12-20 18:43:13 +0200

    scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+
+        
+        $(document).ready(function() {
+            $("#Quiz form").submit(function(event) {
+              var Quiz = ["Answer1", "Answer2", "Answer3"];
+        
+              Quiz.forEach(function(Quiz) {
+                var userInput = $("input#" + Quiz).val();
+                $("." + Quiz).text(userInput);
+              });
+        
+              $("#story").show();
+        
+              event.preventDefault();
+            });
+          }); 

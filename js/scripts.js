933cb9c563400589465ab235384843716f575641
Gahozipauline <gashozipauly@gmail.com>
2018-12-21 15:21:16 +0200

    changes in html

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,28 @@
 
         
 
-          Function Quiz()
+          $(document).ready(function() 
           {
-          var Answer1=document.forms["Answer"].value;
-          var Answer2=document.forms["Answer2"].value;
-          var Answer3=document.forms["Answer3"].value;
-          var Answer4=document.forms["Answer4"].value;
+          var A1=document.forms["form-control"]["answer"].value;
+          var A2=document.forms["form-control"]["answer2"].value;
+          var A3=document.forms["form-contol"]["answer3"].value;
+          var A4=document.forms["form-control"]["answer4"].value;
         
-          var Quiz=[Answer1,Answer2,Answer3,Answer4];
-          if(Answer1=="Livewire")
+          var Quiz=[0];
+          if(A1=="Livewire")
           {
            marks=marks+5;
            }
-          if(Answer2=="storing numbers,dates,or other values")
+          if(A2=="storing numbers,dates,or other values")
           {
            marks=marks+5;
            }
-          if(Answer3=="Javascript syntax is loosely based on java's")
+          if(A3=="Javascript syntax is loosely based on java's")
           {
            marks=marks+5;
           }
-          if(Answer4=="Yes")
+          if(A4=="Yes")
+          marks=marks+5;
           {
            
              alert("your score is:"+marks);

d0948042592deaf5047e1dec6f97d39ccaf34c8d
Gahozipauline <gashozipauly@gmail.com>
2018-12-21 14:33:35 +0200

    html changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,30 @@
 
         
-        $(document).ready(function() {
-            $("#Quiz form").submit(function(event) {
-              var Quiz = ["Answer1", "Answer2", "Answer3","Answer4"];
+
+          Function Quiz()
+          {
+          var Answer1=document.forms["Answer"].value;
+          var Answer2=document.forms["Answer2"].value;
+          var Answer3=document.forms["Answer3"].value;
+          var Answer4=document.forms["Answer4"].value;
         
-              Quiz.forEach(function(Quiz) {
-                var userInput = $("input#" + Quiz).val();
-                $("." + Quiz).text(userInput);
-              });
-             var Quiz= $("#Answer-showing").toggle();
-             var Quiz=$("#Answer-hidden").toggle();
-               event.preventDefault();
-            });
-          }); 
+          var Quiz=[Answer1,Answer2,Answer3,Answer4];
+          if(Answer1=="Livewire")
+          {
+           marks=marks+5;
+           }
+          if(Answer2=="storing numbers,dates,or other values")
+          {
+           marks=marks+5;
+           }
+          if(Answer3=="Javascript syntax is loosely based on java's")
+          {
+           marks=marks+5;
+          }
+          if(Answer4=="Yes")
+          {
+           
+             alert("your score is:"+marks);
+          
+          }
+      
